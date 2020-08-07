76d1b04818b5916a2740f94ad1220759e9b5bf9b
Bratin Kundu <bratin.kundu@gatewaygroup.com>
2020-08-07 11:13:15 +0530

    Added auth user cloud function

--- Backend/functions/Controllers/todo.js
+++ Backend/functions/Controllers/todo.js
@@ -6,3 +6,42 @@ const app = express();
 
 app.use(cors ({ origin: true }));
 
+exports.createAuthUser = functions.https.onCall(async (data)=> {
+    const userEmail = data.email
+    const name = data.name
+    const password = data.password
+    const phno = data.phoneno
+
+    return admin.auth().createUser({
+        email: userEmail,
+        password: password
+    })
+    .then((userdata) => {
+        return admin.firestore().collection("Users").doc(userdata.uid).set({
+            id: userdata.uid,
+            name: name,
+            email: userEmail,
+            contactno: phno
+        })
+        .then(()=>{
+            return{
+                success: true,
+                msg: 'New user created succesfully!' 
+            }
+        })
+        .catch(error =>{
+            return {
+                success : false,
+                msg: 'Adding user failed. Please try again',
+                error: error
+            }
+        })
+    })
+    .catch(error => {
+        return{
+            success: false,
+            msg: 'Error in authenticating user',
+            error: error.message
+        }
+    })
+});
