aef903d93745f94bcd745fd397033f956cf8b9b6
Bratin Kundu <bratin.kundu@gatewaygroup.com>
2020-08-08 11:48:14 +0530

    Added login page and edit todo cloud function

--- Frontend/TodoFrontend/src/app/auth/auth.module.ts
+++ Frontend/TodoFrontend/src/app/auth/auth.module.ts
@@ -1,14 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { MatFormFieldModule } from '@angular/material/form-field';
+import {  MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 import { AuthRoutingModule } from './auth-routing.module';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [LoginComponent, RegisterComponent],
   imports: [
     CommonModule,
-    AuthRoutingModule
+    AuthRoutingModule,
+    MatFormFieldModule,
+    MatButtonModule,
+    MatIconModule,
+    MatInputModule
   ]
 })
 export class AuthModule { }
