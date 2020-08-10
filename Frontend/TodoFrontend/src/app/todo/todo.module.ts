6ad67f750621d8b6f4f4e240b1a00d5439d7cedf
Bratin Kundu <bkundu53@gmail.com>
2020-08-10 19:10:44 +0530

    Todo Component with dialog

--- Frontend/TodoFrontend/src/app/todo/todo.module.ts
+++ Frontend/TodoFrontend/src/app/todo/todo.module.ts
@@ -2,19 +2,23 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
-import { MatCardModule } from '@angular/material/card'
+import { MatCardModule } from '@angular/material/card';
+import { MatDialogModule, MatDialogRef } from '@angular/material/dialog';
 import { TodoRoutingModule } from './todo-routing.module';
 import { TodocomponentComponent } from './todocomponent/todocomponent.component';
+import { TodoDialog } from './todocomponent/todocomponent.component';
 
 
 @NgModule({
-  declarations: [TodocomponentComponent],
+  declarations: [TodocomponentComponent, TodoDialog],
   imports: [
     CommonModule,
     TodoRoutingModule,
     MatButtonModule,
     MatIconModule,
-    MatCardModule
-  ]
+    MatCardModule,
+    MatDialogModule
+  ],
+  entryComponents:[TodoDialog]
 })
 export class TodoModule { }
--- Frontend/TodoFrontend/src/app/todo/todocomponent/todocomponent.component.ts
+++ Frontend/TodoFrontend/src/app/todo/todocomponent/todocomponent.component.ts
@@ -1,4 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
+import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material/dialog';
+
+export interface TodoInterface{
+  title : string;
+  details : string;
+}
+
 
 @Component({
   selector: 'app-todocomponent',
@@ -7,9 +14,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TodocomponentComponent implements OnInit {
 
-  constructor() { }
+
+  constructor(private dialog: MatDialog) { }
 
   ngOnInit() {
+  }
+
+  openDialog() : void{
+    const dialogRef = this.dialog.open(TodoDialog,{
+      width: '500px',
+      data: {}
+    });
+
+    dialogRef.afterClosed().subscribe(result =>{
+      //get the return data here
+      console.log("Dialog closed")
+    })
+  }
+
+}
+
+
+@Component({
+  selector:'app-dialog',
+  templateUrl: 'dialog.html',
+})
+export class TodoDialog{
+  constructor(public dialogRef: MatDialogRef<TodoDialog>, @Inject(MAT_DIALOG_DATA) public data : TodoInterface){}
+
+  onNoClick() : void {
+    this.dialogRef.close();
   }
 
 }
