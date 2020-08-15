fa65c9b6bd46828ddf9020bb6381b77577fcb37f
bratinkundu <bkundu53@gmail.com>
2020-08-15 13:49:21 +0530

    Added working API

--- Frontend/TodoFrontend/src/app/todo/todo.module.ts
+++ Frontend/TodoFrontend/src/app/todo/todo.module.ts
@@ -3,7 +3,11 @@ import { CommonModule } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatCardModule } from '@angular/material/card';
+import {ReactiveFormsModule} from '@angular/forms';
+import { MatRippleModule } from '@angular/material/core';
 import { MatDialogModule, MatDialogRef } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import {  MatInputModule } from '@angular/material/input';
 import { TodoRoutingModule } from './todo-routing.module';
 import { TodocomponentComponent } from './todocomponent/todocomponent.component';
 import { TodoDialog } from './todocomponent/todocomponent.component';
@@ -17,7 +21,11 @@ import { TodoDialog } from './todocomponent/todocomponent.component';
     MatButtonModule,
     MatIconModule,
     MatCardModule,
-    MatDialogModule
+    MatFormFieldModule,
+    MatInputModule,
+    MatDialogModule,
+    ReactiveFormsModule,
+    MatRippleModule
   ],
   entryComponents:[TodoDialog]
 })
--- Frontend/TodoFrontend/src/app/todo/todocomponent/todocomponent.component.ts
+++ Frontend/TodoFrontend/src/app/todo/todocomponent/todocomponent.component.ts
@@ -1,9 +1,14 @@
-import { Component, OnInit, Inject } from '@angular/core';
+import { Component, OnInit, Inject, OnDestroy } from '@angular/core';
 import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material/dialog';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { TodoService } from 'src/app/services/todo.service';
+import { Subscription } from 'rxjs';
 
 export interface TodoInterface{
+  id : string;
   title : string;
   details : string;
+  operation : string;
 }
 
 
@@ -12,24 +17,100 @@ export interface TodoInterface{
   templateUrl: './todocomponent.component.html',
   styleUrls: ['./todocomponent.component.css']
 })
-export class TodocomponentComponent implements OnInit {
+export class TodocomponentComponent implements OnInit, OnDestroy {
 
 
-  constructor(private dialog: MatDialog) { }
+  constructor(private dialog: MatDialog, private todoservice : TodoService) { }
 
+  private subs = new Subscription();
+  allTodos = []
   ngOnInit() {
+    this.getAllTodoList();
   }
 
-  openDialog() : void{
+  getAllTodoList(){
+    let tododata ={
+      data:{
+        uid : "fSD2ipRSuXP3mZ9dynrLJ5lbsIb2"
+      }
+    }
+    this.subs.add(this.todoservice.getAllTodo(tododata).subscribe(
+      result=>{
+        this.allTodos = result['result'];
+      }))
+  }
+
+
+  editSpecificTodo(result){
+    let tododata = {
+      data :{
+        uid : "fSD2ipRSuXP3mZ9dynrLJ5lbsIb2",
+        docid : result.id,
+        details : result.details
+      }
+    }
+
+    this.subs.add(this.todoservice.editTodo(tododata).subscribe(
+      response=>{
+        console.log(response);
+        alert('Updated the todo!');
+        this.ngOnInit();
+      }))
+  }
+
+
+  addTodo(result){
+    let tododata = {
+      data : {
+        uid : "fSD2ipRSuXP3mZ9dynrLJ5lbsIb2",
+        tododetails : result.details
+      }
+    }
+    this.subs.add(this.todoservice.addTodo(tododata).subscribe(
+      response =>{
+        console.log(response)
+        alert('Todo added successfully!')
+        this.ngOnInit();
+    }))
+  }
+
+  openDialog(todo) : void{
+    let tododata;
+    if(todo.id){
+      tododata = {
+        id : todo.id,
+        title : "",
+        details : todo.details,
+        operation : "EditTodo"
+      }
+    }
+    else{
+      tododata ={
+        operation : "AddTodo"
+      }
+    }
+
     const dialogRef = this.dialog.open(TodoDialog,{
       width: '500px',
-      data: {}
+      data: tododata
     });
 
-    dialogRef.afterClosed().subscribe(result =>{
+     dialogRef.beforeClose().subscribe(result =>{
       //get the return data here
-      console.log("Dialog closed")
-    })
+      if(result.id != ""){
+        this.editSpecificTodo(result);
+      }
+      else{
+        this.addTodo(result);
+      }
+    });
+  }
+
+  ngOnDestroy(){
+    if(this.subs){
+      this.subs.unsubscribe();
+    }
+    
   }
 
 }
@@ -39,8 +120,25 @@ export class TodocomponentComponent implements OnInit {
   selector:'app-dialog',
   templateUrl: 'dialog.html',
 })
-export class TodoDialog{
-  constructor(public dialogRef: MatDialogRef<TodoDialog>, @Inject(MAT_DIALOG_DATA) public data : TodoInterface){}
+export class TodoDialog implements OnInit{
+  constructor(private formbuilder : FormBuilder,public dialogRef: MatDialogRef<TodoDialog>, @Inject(MAT_DIALOG_DATA) public data : TodoInterface){}
+
+  todoForm : FormGroup
+
+  ngOnInit(){
+    this.todoForm = this.formbuilder.group({
+      id : [""],
+      title : ["", Validators.required],
+      details : ["", Validators.required]
+    })
+
+    if(this.data.operation === "EditTodo"){
+      this.todoForm.get('details').setValue(this.data.details);
+      this.todoForm.get('id').setValue(this.data.id);
+    }
+  }
+
+
 
   onNoClick() : void {
     this.dialogRef.close();
